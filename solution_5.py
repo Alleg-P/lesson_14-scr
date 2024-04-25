fe8cecb8411f6a567dc408ceea8881823b3e831f
Alleg <opankov@inbox.ru>
2024-04-25 19:17:04 +0300

    fixed_2

--- solution_5.py
+++ solution_5.py
@@ -1,12 +1,19 @@
 # Задача 5: Инвентаризация товаров
+  
+def count_items(items):
+    total = {}
+    for item in items:
+        total[item[1]] = total.get(item[1], 0) + 1
+    return total
 
-items = [
+input_data = [
     [('Рубашка', 'Одежда'), ('Кружка', 'Посуда')],
     [('Рубашка', 'Одежда'), ('Штаны', 'Одежда'), ('Кружка', 'Посуда')],
     [('Ручка', 'Канцелярия'), ('Тетрадь', 'Канцелярия'), ('Кружка', 'Посуда'), ('Стул', 'Мебель')]
 ]
 
-for item in items:
-    result = list(map(lambda x: (x[1], 1), item))
-    print(result)
+output_data = list(map(lambda x: count_items(x), input_data))
+
+for data in output_data:
+    print(data)
     
